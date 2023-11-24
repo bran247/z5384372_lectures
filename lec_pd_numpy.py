d5c62ae1052ad4f6c1cf0fe255370dd7f08b58e6
bran247 <150109314+bran247@users.noreply.github.com>
2023-11-24 16:16:12 +1100

    Update lec_pd_numpy.py

--- lec_pd_numpy.py
+++ lec_pd_numpy.py
@@ -3,16 +3,15 @@
 Companion codes for the lecture on Numpy
 """
 
-import numpy as np 
+import numpy as np
 import pandas as pd
 
 
-
-# ---------------------------------------------------------------------------- 
+# ----------------------------------------------------------------------------
 #   The dates and prices lists
-# ---------------------------------------------------------------------------- 
+# ----------------------------------------------------------------------------
 dates = [
-  '2020-01-02', 
+  '2020-01-02',
   '2020-01-03',
   '2020-01-06',
   '2020-01-07',
@@ -25,7 +24,7 @@ dates = [
   ]
 
 prices = [
-  7.1600, 
+  7.1600,
   7.1900,
   7.0000,
   7.1000,
@@ -50,9 +49,9 @@ bday = [
   9,
   10]
 
-# ---------------------------------------------------------------------------- 
+# ----------------------------------------------------------------------------
 #   Create two series
-# ---------------------------------------------------------------------------- 
+# ----------------------------------------------------------------------------
 
 # Series with prices
 prc_ser = pd.Series(data=prices, index=dates)
@@ -61,56 +60,56 @@ prc_ser = pd.Series(data=prices, index=dates)
 bday_ser = pd.Series(data=bday, index=dates)
 
 
-# ---------------------------------------------------------------------------- 
+# ----------------------------------------------------------------------------
 #   Create a dataframe
-# ---------------------------------------------------------------------------- 
+# ----------------------------------------------------------------------------
 # Data Frame with close and Bday columns
-df = pd.DataFrame({'Close': prc_ser, 'Bday': bday_ser}) 
-#print(df) 
+df = pd.DataFrame({'Close': prc_ser, 'Bday': bday_ser})
+print(df)
 
-#df.info() 
+df.info()
 
 # Get the series containing "Close" prices
-#ser = df['Close'] 
+ser = df['Close']
 
 # Get the underlying data array
-#print(ser.array) 
+print(ser.array)
 
 # and the type
-#print(type(ser.array)) 
+print(type(ser.array))
 
 
-# The .values attribute will give you a numpy array 
+# The .values attribute will give you a numpy array
 # with the contents of the series
-#print(ser.values) 
+print(ser.values)
 
 # The type is <class 'numpy.ndarray'>
-#print(type(ser.values)) 
+print(type(ser.values))
 
 # ----------------------------------------------------------------------------
-#   Working with missing data 
+#   Working with missing data
 # ----------------------------------------------------------------------------
 
 # Add an empty row to the `df` dataframe
 # Create a copy
-#df_nan = df.copy() 
+df_nan = df.copy()
 
 # Add an emtpy row to the `df_nan` dataframe
-#df_nan.loc['3000-01-01'] = [np.nan, np.nan] 
-#print(df_nan) 
+df_nan.loc['3000-01-01'] = [np.nan, np.nan]
+print(df_nan)
 
 # Note that the dtypes changed
-#print("\nThis is the `df` dataframe:") 
-#print(df.info()) 
-#print("\nThis is the `df_nan` dataframe:") 
-#print(df_nan.info()) 
+print("\nThis is the `df` dataframe:")
+print(df.info())
+print("\nThis is the `df_nan` dataframe:")
+print(df_nan.info())
 
 # Convert dtypes
-#df_new = df_nan.convert_dtypes() 
-#print(df_new.info()) 
+df_new = df_nan.convert_dtypes()
+print(df_new.info())
 
-#print(df_new.loc['3000-01-01']) 
-#print(type(df_new.loc['3000-01-01', 'Bday'])) 
+print(df_new.loc['3000-01-01'])
+print(type(df_new.loc['3000-01-01', 'Bday']))
 
 
 # ----------------------------------------------------------------------------
@@ -131,7 +130,6 @@ df = pd.DataFrame({'Close': prc_ser, 'Bday': bday_ser})
 #print('\ndf0.info() --> ')
 #df0.info()
 #
-
 
 
 # A data frame with a strings as index labels
